4dba395fbbb19719434e72b5ad94ebf053ef545a
chris-daynes <chrisdaynes@gmail.com>
2016-11-19 21:42:18 +1300

    adding total books on one line

--- views/cart.js
+++ views/cart.js
@@ -1,10 +1,8 @@
 var h = require('hyperscript')
 
 module.exports = ({cart, products}, dispatch) => {
-  const keys = Object.keys(cart)
-  const values = keys.map((key) => cart[key])
-  let totalBooks = values.reduce((a, b) => a + b , 0)
-  let totalPrice =
+  const totalBooks = Object.keys(cart).reduce((sum, id) => sum + cart[id] , 0)
+
   console.log('The total books are: ', totalBooks);
   return h('div#cart', [
     totalBooks,
